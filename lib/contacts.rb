d7f768682d7b4522a01e108903dec0022890dc4b
Chris Houghton <chrishoughton2434@gmail.com>
2020-09-18 13:55:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -5,7 +5,8 @@ def remove_strawberry(contacts)
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+          data.each do |yay|
+            puts yay
         end
       end
     end
